f0f3402a7532c85e2f67fcd0ac87d5133eb73421
Jun <kjoon418@naver.com>
2024-10-03 20:17:59 +0900

    Feat: 스프링 DB 1편에 대한 프로젝트 생성

---
+++ SpringDB1/src/main/java/com/example/springdb1/SpringDb1Application.java
@@ -0,0 +1,13 @@
+package com.example.springdb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDb1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDb1Application.class, args);
+    }
+
+}
---
+++ SpringDB1/src/test/java/com/example/springdb1/SpringDb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDb1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
